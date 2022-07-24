966d5eced25e42b4cb3c47e9e1a910a1f5f178ec
Hank Shen <swh@admpub.com>
2022-07-24 21:18:08 +0800

    update

---
+++ docker/start_run.sh
@@ -0,0 +1,9 @@
+image="test-ubuntu-firewall"
+if [ "$1" != "" ]; then
+    image="$1"
+fi
+docker run --rm -it\
+ --privileged\
+ --entrypoint go\
+ -p "18080:8080" -v "$GOPATH/src:/root/go/src" $image\
+ run . -tags sqlite -c /myconfig/config.yaml -p 8080
---
+++ docker/start_tower.sh
@@ -0,0 +1,5 @@
+image="test-ubuntu-firewall"
+if [ "$1" != "" ]; then
+    image="$1"
+fi
+docker run --rm -it --privileged -p "18080:8080" -v "$GOPATH/src:/root/go/src" $image
