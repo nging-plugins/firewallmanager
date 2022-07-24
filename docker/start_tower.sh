d6f987e2831917f604236e37e7e32ea72d318044
Hank Shen <swh@admpub.com>
2022-07-24 22:19:31 +0800

    update

--- docker/start_tower.sh
+++ docker/start_tower.sh
@@ -2,4 +2,4 @@ image="test-ubuntu-firewall"
 if [ "$1" != "" ]; then
     image="$1"
 fi
-docker run --rm -it --privileged -p "18080:8080" -v "$GOPATH/src:/root/go/src" $image
+docker run --rm -it --privileged -p "18080:8080" -v "$GOPATH/src:/root/go/src" $image -run.appendParams "-c /myconfig/config.yaml"
