e26ca4c086a678201b2e17c76670e5c1368b1ce4
Hank Shen <swh@admpub.com>
2022-11-12 13:04:59 +0800

    update

--- example/runindocker.sh
+++ example/runindocker.sh
@@ -5,6 +5,6 @@ fi
 docker run --rm -it\
  --workdir /root/go/src/github.com/nging-plugins/firewallmanager/example\
  --privileged\
- --network host\
+ -p "28181:28181"\
  -v "$GOPATH/src:/root/go/src" -v "$GOPATH/src:$GOPATH/src" $image
 # 在容器内通过 host.docker.internal 访问宿主机
